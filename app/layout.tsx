bb515ddcdf94410ed31ab4e259d9d0eca9890875
Nitesh-DK <nittesh.dk@gmail.com>
2025-05-07 20:36:18 +0530

    done everything

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
           <Navbar />
            
           {children}
-          {/* <Footer /> */}
+          <Footer />
          </main>
         </ThemeProvider>
         <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" strategy="beforeInteractive" />
